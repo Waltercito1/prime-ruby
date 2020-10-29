cc7a0858fce8aa9ce4dc43ac6c906c14fa20431c
Walter Aab <aabwalter@yahoo.com>
2020-10-28 19:15:07 -0500

    commit tree a9359977a08519fd46873b3ec7037d946d5f8ed0

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+#A prime number is a natural number greater than 1 that is not a product of two smaller natural numbers.
+=begin
+def prime?(n)
+    return false if n < 2
+    (2..n - 1).each do |x|
+        if (n % x) == 0
+            return false
+        end
+    end
+    true
+end
+=end
+
+#or an alternative, starting out with a variable set to true.
+#=begin
+def prime?(n)
+    return false if n < 2
+    (2..n - 1).each {|x| return false if (n % x) == 0 }
+    true
+end
+#=end
